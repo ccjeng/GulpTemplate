2dbafc99b6ab9b77d83f568502ac9e3714d39d6e
andycheng <ccjeng@gmail.com>
2017-03-25 14:19:42 +0800

    Renew gulp packages and gulpfile.js

--- README.md
+++ README.md
@@ -2,12 +2,37 @@
 -----------------
 This is a gulp template which I am currently using for web development.
 
+**Feature**
+- Minify javascript, css and images
+- Deploy to Github page
+
 **Install**
- 1. install node.js
- 2. install global gulp
- `npm install -g gulp`
- 3. install gulp packages
- `sudo npm install gulp gulp-clean gulp-connect gulp-watch gulp-minify-css gulp-concat gulp-uglify gulp-imagemin gulp-livereload gulp-sourcemaps gulp-notify gulp-gh-pages --save-dev`
+1. install node.js
+2. install related packages
+```
+ npm install
+```
+
+(Option) Install all gulp packages manually
+```
+sudo npm install gulp gulp-clean gulp-connect gulp-watch gulp-minify-css gulp-concat gulp-uglify gulp-imagemin gulp-livereload gulp-sourcemaps gulp-notify gulp-gh-pages --save-dev
+```
+
+**Usage**
+- Run  (Access development web via http://locahost:8080)
+```
+npm start
+```
+
+- Build
+```
+npm build
+```
+- Deploy to Github Page
+```
+npm deploy
+```
 
 **License**
+
 http://ccjeng.mit-license.org
--- gulpfile.js
+++ gulpfile.js
@@ -9,75 +9,101 @@ var livereload = require('gulp-livereload');
 var clean = require('gulp-clean');
 var sourcemaps = require('gulp-sourcemaps');
 var notify = require('gulp-notify');
+var ghPages = require('gulp-gh-pages');
 
-// Error Helper
-function onError(err) {
-    console.log(err);
-}
+const dirs = {
+  src: 'src',
+  dest: 'server'
+};
+
+const stylesPaths = {
+  src: dirs.src + '/css/*.css',
+  dest: dirs.dest + '/css'
+};
+
+const scriptsPaths = {
+  src: dirs.src + '/js/*.js',
+  dest: dirs.dest + '/js'
+};
+
+const imagesPaths = {
+  src: dirs.src + '/img/*',
+  dest: dirs.dest + '/img'
+};
+
 
 // Server Task
 gulp.task('server', function() {
     connect.server({
-        root: 'server',
-        livereload: true
+        root: dirs.dest,
+        livereload: true,
+        port: 8080
     });
 });
 
 // Styles Task
 gulp.task('styles', function() {
-    return gulp.src('src/css/*.css')
+    return gulp.src(stylesPaths.src)
         .pipe(sourcemaps.init())
         .pipe(concat('all.min.css'))
         .pipe(cssminify())  
         .pipe(sourcemaps.write('./'))      
-        .pipe(gulp.dest('server/css'))
-        .pipe(notify({ message: 'Styles task complete' }));
+        .pipe(gulp.dest(stylesPaths.dest))
+        .pipe(connect.reload());
 });
 
 gulp.task('html', function () {
-    return gulp.src('src/*.html')
-        .pipe(gulp.dest('server'))
-        .pipe(notify({ message: 'Html task complete' }));
+    return gulp.src('${dirs.src}/*.html')
+        .pipe(gulp.dest('${dirs.dest}'))
+        .pipe(connect.reload());
 });
 
 // Scripts Task
 gulp.task('scripts', function() {
-    return gulp.src('src/js/*.js')
-        .pipe(sourcemaps.init())
+    return gulp.src(scriptsPaths.src)
+        .pipe(sourcemaps.init({loadMaps: true}))
         .pipe(concat('all.min.js'))
         .pipe(uglify())
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('server/js'))
-        .pipe(notify({ message: 'Styles task complete' }));
+        .pipe(gulp.dest(scriptsPaths.dest));
 });
 
 // Images Task
 gulp.task('images', function() {
-    return gulp.src('src/img/*')
+    return gulp.src(imagesPaths.src)
         .pipe(imagemin())
-        .pipe(gulp.dest('server/img'))
-        .pipe(notify({ message: 'Images task complete' }));
+        .pipe(gulp.dest(imagesPaths.dest));
 });
 
 
 // Clean Task
 gulp.task('clean', function() {
-    return gulp.src(['server/css/*', 'server/js/*, server/img/*'], {read: false})
+    return gulp.src([stylesPaths.dest+'/*', scriptsPaths.dest+'/*', imagesPaths.dest+'/*'], {read: false})
         .pipe(clean());
 });
 
 // Watch Task
 gulp.task('watch', function() {
     gulp.watch('src/*.html', ['html']);
-    gulp.watch('src/css/*.css', ['styles']);
-    gulp.watch('src/js/*.js', ['scripts']);
-    gulp.watch('src/img/*', ['images']);
+    gulp.watch(stylesPaths.src, ['styles']);
+    gulp.watch(scriptsPaths.src, ['scripts']);
+    gulp.watch(imagesPaths.src, ['images']);
 
     // Watch any files in server/, reload on change
     livereload.listen();
     gulp.watch(['server/**']).on('change', livereload.changed);
 });
 
+//Deploy to ghPages Task
+gulp.task('ghpages', ['build'], function() {
+  return gulp.src(dirs.dest+'/**/*')
+    .pipe(ghPages());
+});
+
 // Default Task
 gulp.task('default', ['clean', 'html', 'styles', 'scripts', 'images', 'server', 'watch']);
+gulp.task('build', ['clean', 'html', 'scripts', 'styles', 'images']);
+gulp.task('deploy', ['ghpages']);
+
+
 
